a63579828addc8be27bcec50d70d4fbd6625ee55
maxcrimea <maxcrimea50@gmail.com>
2023-10-20 02:23:14 +0600

    added swagger

--- library/urls.py
+++ library/urls.py
@@ -1,8 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from library import swagger
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('books.urls')),
 ]
+urlpatterns += swagger.urlpatterns
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
