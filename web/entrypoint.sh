43795c3f58584418c81f8a5380196306009be1a5
maxcrimea <maxcrimea50@gmail.com>
2023-10-21 01:30:13 +0600

    added README

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -11,8 +11,9 @@ python manage.py migrate
 
 python manage.py collectstatic --noinput
 
-echo "from django.contrib.auth.models import User
-if not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists():
-    User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')" | python manage.py shell
+
+echo "from django.contrib.auth.models import User; User.objects.create_superuser(
+    '$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD'
+    )  if not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists() else pass"
 
 uwsgi --ini /app/library.uwsgi.ini
